68205de0032f4cf05129e3b23eeccb3469da80c3
Aymane <aymanecheikh2002@gmail.com>
2025-11-23 22:39:50 +0000

    more cpp practice

--- cpp_notes.cpp
+++ cpp_notes.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <cmath>
 #include <climits>
+#include <float.h>
+#include <string>
 
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::boolalpha;
+using std::fixed;
+using std::string;
 
 
 double power(double base, int exponent)
@@ -37,31 +42,94 @@ void power_function()
 
 void integral_limit_constants()
 {
+	cout << "Single quotes for char... double quotes for strings."
+	<< endl;
+	
+	char x = 'A';
+	cout << x << endl;
+	cout << (int) x << endl;
+
 	cout << "Short Min: " << SHRT_MIN << endl;
 	cout << "Short Max: " << SHRT_MAX << endl;
 	cout << "Unsigned Short Max: " << USHRT_MAX << endl << endl;
-	
+
 	cout << "Int Min: " << INT_MIN << endl;
 	cout << "Int Max: " << INT_MAX << endl;
 	cout << "Unsigned Int Max: " << UINT_MAX << endl << endl;
-	
+
 	cout << "Long Min: " << LONG_MIN << endl;
 	cout << "Long Max: " << LONG_MAX << endl;
 	cout << "Unsigned Long Max: " << ULONG_MAX << endl << endl;
-	
+
 	cout << "Long Long Min: " << LLONG_MIN << endl;
 	cout << "Long Long Max: " << LLONG_MAX << endl;
 	cout << "Unsigned Long Long Max: " << ULLONG_MAX << endl << endl;
 }
 
-int main()
+void bool_study()
 {
-	power_function();
 	cout << endl;
 	integral_limit_constants();
-	cout << "I also covered chars but haven't yet addressed it explicitly"
-	<< " here." << endl << "I also need to cover bools and maybe escape "
-	<< "sequences if I have time." << endl << "But this is secondary to "
-	<< "my focus. I will continue on and cover all other data types."
-	<< endl;
+	bool found = false;
+	cout << boolalpha << found << endl;
+	found = true;
+	cout << boolalpha << found << endl;
+	
+}
+
+void float_study()
+{
+	float a = 10.0 / 3;
+	double b = 10.0 / 3;
+	long double c = 10.0 / 3;
+	a = a * 10000000000000;
+	b = b * 10000000000000;
+	c = c * 10000000000000;
+
+	cout << fixed << a << endl;
+	cout << FLT_DIG << endl;
+	cout << fixed << b << endl;
+	cout << DBL_DIG << endl;
+	cout << fixed << c << endl;
+	cout << LDBL_DIG << endl;
+
+}
+
+void constants()
+{
+	const int SETTING = 5;
+	enum { y = 100 };
+
+}
+
+void math_funcs()
+{
+	cout << sqrt(-25) << endl;
+	cout << pow(9, 999) << endl;
+	cout << pow(-9, 999) << endl;
+	cout << NAN << endl;
+	cout << INFINITY << endl;
+	cout << -INFINITY << endl;
+	cout << remainder(10, 3) << endl;
+	cout << 10 % 3 << endl;
+	cout << remainder(10, 3.25) << endl;
+	cout << fmod(10, 3.25) << endl;
+	cout << fmax(10, 3.25) << endl;
+	cout << fmin(10, 3.25) << endl;
+	cout << ceil(fmin(10, 3.25)) << endl;
+	cout << floor(fmin(10, 3.25)) << endl;
+	cout << trunc(fmin(10, 3.25)) << endl;
+
+}
+
+int main()
+{
+	string greeting = "hello";
+	string complete_greeting = greeting + " there";
+	complete_greeting += "!";
+	cout << complete_greeting << endl;
+	cout << complete_greeting.length() << endl;
+	string greeting_getlined;
+	getline(cin, greeting_getlined);
+	cout << greeting_getlined << endl;
 }
